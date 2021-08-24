33f1b3d5e91fee9c2bc5c4a1d225dd8587a6c825
41SergioF <carlossergio2401@outlook.com>
2021-08-24 01:36:52 -0300

    User entity and resource

---
+++ webservices/src/main/java/com/carlossergio/webservices/resources/UserResource.java
@@ -0,0 +1,30 @@
+package com.carlossergio.webservices.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.carlossergio.webservices.entities.User;
+
+/*Esse classe é um recurso web implementado pelo controlador REST
+ * 
+ * Esse é um nome para o recurso. Nome do recurso "/users"
+ * */
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	/*Esse método é um EndPoint para acessar os usuários
+	 * */
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "85 98653298", "123456");
+		/*Esse método vai retornar um objeto do tipo ResponseEntity.
+		 * .ok diz que deu tudo certo.
+		 * .body manda o corpo do user
+		 * */
+		return ResponseEntity.ok().body(user);
+	}
+	
+}
