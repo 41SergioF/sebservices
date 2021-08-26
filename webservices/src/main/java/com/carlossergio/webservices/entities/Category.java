638a30faa6928655ca1a3c26093b31202e9c3954
41SergioF <carlossergio2401@outlook.com>
2021-08-25 23:42:59 -0300

    Many-to-many association with JoinTable

--- webservices/src/main/java/com/carlossergio/webservices/entities/Category.java
+++ webservices/src/main/java/com/carlossergio/webservices/entities/Category.java
@@ -9,8 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -27,7 +29,8 @@ public class Category implements Serializable {
 	/*
 	 * @Transient: Esse diz ao Spring que iginore a linha abaixo 
 	 */
-	@Transient
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>(); 
 	
 	public Category() {}
--- webservices/src/main/java/com/carlossergio/webservices/entities/Product.java
+++ webservices/src/main/java/com/carlossergio/webservices/entities/Product.java
@@ -9,28 +9,37 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
-	@Id 
+
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private double price;
 	private String imgUrl;
-	
-	@Transient
+
+	/*
+	 * @JoinTable(): Faz a associação entre duas tabelas Vai precisar do nome, e
+	 * quais colunas devem ser associadas
+	 */
+
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
-	public Product() {}
-	
+
+	public Product() {
+	}
+
 	public Product(Long id, String name, String description, double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
@@ -82,7 +91,7 @@ public class Product implements Serializable{
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -99,5 +108,5 @@ public class Product implements Serializable{
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-		
+
 }
