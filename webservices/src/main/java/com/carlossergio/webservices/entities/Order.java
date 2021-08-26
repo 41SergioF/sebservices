22fe484c8d81afe79e91ac4917f576d59e492180
41SergioF <carlossergio2401@outlook.com>
2021-08-25 22:05:46 -0300

    OrderStatus enum

--- webservices/src/main/java/com/carlossergio/webservices/entities/Order.java
+++ webservices/src/main/java/com/carlossergio/webservices/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.carlossergio.webservices.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 /*
@@ -31,6 +32,7 @@ public class Order implements Serializable{
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 	
 	/*
 	 * Implementa o muitos para um da UML
@@ -41,9 +43,10 @@ public class Order implements Serializable{
 
 	public Order() {}
 	
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -61,6 +64,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}			
 	}
 
 	public User getClient() {
