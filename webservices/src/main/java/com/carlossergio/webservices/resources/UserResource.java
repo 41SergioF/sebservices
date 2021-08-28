f82243216b583456a03c322e4aa638ebe6f3af02
41SergioF <carlossergio2401@outlook.com>
2021-08-28 12:45:07 -0300

    User update

--- webservices/src/main/java/com/carlossergio/webservices/resources/UserResource.java
+++ webservices/src/main/java/com/carlossergio/webservices/resources/UserResource.java
@@ -3,14 +3,13 @@ package com.carlossergio.webservices.resources;
 import java.net.URI;
 import java.util.List;
 
-import javax.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,29 +40,36 @@ public class UserResource {
 	}
 
 	/*
-	 * Esse trexo "(value = "/{id}")", diz que o resource
-	 * receberá uma valor de Id
+	 * Esse trexo "(value = "/{id}")", diz que o resource receberá uma valor de Id
 	 */
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User user = userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
-	
-	
+
 	@PostMapping
-	public ResponseEntity<User> insert(@RequestBody User obj){
+	public ResponseEntity<User> insert(@RequestBody User obj) {
 		obj = userService.insert(obj);
-		///Forma padrão de criação de endereços
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		/// Forma padrão de criação de endereços
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 	}
+
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id){
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		userService.delete(id);
-		///Para respostas vazias
+		/// Para respostas vazias
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	/*
+	 * @PutMapping: Para atualizações(update)
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = userService.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
--- webservices/src/main/java/com/carlossergio/webservices/service/UserService.java
+++ webservices/src/main/java/com/carlossergio/webservices/service/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.IncorrectUpdateSemanticsDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.carlossergio.webservices.entities.User;
@@ -33,5 +34,17 @@ public class UserService {
 
 	public void delete(Long id) {
 		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
