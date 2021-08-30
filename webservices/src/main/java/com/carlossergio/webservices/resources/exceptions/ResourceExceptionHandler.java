4b8e0ef65eca391b12afcc97a2dd5fbbbb723de1
41SergioF <carlossergio2401@outlook.com>
2021-08-29 22:04:30 -0300

    Exception handling - delete

--- webservices/src/main/java/com/carlossergio/webservices/resources/exceptions/ResourceExceptionHandler.java
+++ webservices/src/main/java/com/carlossergio/webservices/resources/exceptions/ResourceExceptionHandler.java
@@ -9,21 +9,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.carlossergio.webservices.service.DatabaseException;
 import com.carlossergio.webservices.service.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	/* @ExceptionHandler(ResourceNotFoundException.class):
-	 * Essa anotação diz que qualquer excessão do tipo que está especificado dentro do parentese
-	 * deve ser capturada aqui.	
-	 * */
+	/*
+	 * @ExceptionHandler(ResourceNotFoundException.class): Essa anotação diz que
+	 * qualquer excessão do tipo que está especificado dentro do parentese deve ser
+	 * capturada aqui.
+	 */
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Databese error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
 }
--- webservices/src/main/java/com/carlossergio/webservices/service/UserService.java
+++ webservices/src/main/java/com/carlossergio/webservices/service/UserService.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.carlossergio.webservices.entities.User;
@@ -33,9 +36,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = userRepository.getOne(id);
 		updateData(entity, obj);
